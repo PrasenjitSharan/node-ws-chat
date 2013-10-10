ad2c2da1c3e7a9d71332e49a05388fd95bfa3e77
Prasenjit Sharan <prasenjit.sharan@gmail.com>
2013-10-11 00:16:55 +0530

    Updated public, view and server. Working version.

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var express = require('express'), app = express.createServer();
+var appPort = process.env.PORT || 3000;
+var express = require('express'), app = express();
 var jade = require('jade');
-var io = require('socket.io').listen(app);
-app.set('views', __direname + '/views');
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout : false });
 app.configure(function(){
-	app.use(express.static(__direname + '/public'));
+	app.use(express.static(__dirname + '/public'));
 });
 app.get('/', function(req, res){
 	res.render('home.jade');
 });
-app.listen(3000);
+server.listen(appPort);
 io.sockets.on('connection', function(socket) {
 	socket.on('setPseudo', function(data) {
 		socket.set('pseudo', data);
